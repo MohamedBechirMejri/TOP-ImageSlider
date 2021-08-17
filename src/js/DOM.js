3e685697f876e859c66c4d45549bade8e0f8db68
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-17 19:07:57 +0100

    remove current pic when I click a buttton

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,17 +1,26 @@
 const createCarousel = (array) => {
-    const carouselDiv = document.getElementById('imgs-container');
-    carouselDiv.innerHTML = '';
-    array.forEach((element) => {
-        const imgDiv = document.createElement('div');
-        imgDiv.classList.add('slideDiv');
-        imgDiv.innerHTML = `<img src="${element.url}" data-id="${array.indexOf(
+  const carouselDiv = document.getElementById('imgs-container');
+  carouselDiv.innerHTML = '';
+  array.forEach((element) => {
+    const imgDiv = document.createElement('div');
+    imgDiv.classList.add('slideDiv');
+    imgDiv.innerHTML = `<img src="${element.url}" data-id="${array.indexOf(
       element,
     )}" class="slide">`;
-        carouselDiv.appendChild(imgDiv);
-    });
-    const bottomNavbar = document.createElement('div');
-    bottomNavbar.classList.add('bottomnavbar');
-    bottomNavbar.innerHTML = carouselDiv.innerHTML;
+    carouselDiv.appendChild(imgDiv);
+  });
+  const bottomNavbar = document.createElement('div');
+  bottomNavbar.classList.add('bottomnavbar');
+  bottomNavbar.innerHTML = carouselDiv.innerHTML;
     carouselDiv.appendChild(bottomNavbar);
+    document.querySelector(`[data-id="0"]`).classList.add('visible')
 };
+
+export const moveToNextPic = () => {
+    const currentPic = document.querySelector('.visible');
+    const currentPicId = currentPic.getAttribute('data-id')
+
+    currentPic.classList.remove('visible')
+};
+
 export default createCarousel;

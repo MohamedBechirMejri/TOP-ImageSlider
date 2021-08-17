f1d49d089946d89f927572c9c1227c1731412f8e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-17 19:06:09 +0100

    fix typo

--- src/js/eventListeners.js
+++ src/js/eventListeners.js
@@ -3,7 +3,7 @@ import { moveToNextPic } from "./DOM";
 const listen = () => {
     // eslint-disable-next-line no-unused-vars
     const toRightButton = (() => {
-        const rightButton = document.getElementById('nav-left');
+        const rightButton = document.getElementById('nav-right');
         rightButton.addEventListener('click', moveToNextPic)
   })();
 };

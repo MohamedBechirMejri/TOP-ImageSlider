4ed7499cfeceaa7258757e43c51483d87d31d212
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-17 17:51:20 +0100

    make createcaropusel function

---
+++ src/js/DOM.js
@@ -0,0 +1,10 @@
+export const createCarousel = (array) => {
+    const carouselDiv = document.getElementById('carousel');
+    array.forEach((element) => {
+        const imgDiv = document.createElement('div');
+        imgDiv.classList.add('slideDiv');
+        imgDiv.innerHTML = `<img src="${element.url}" data-id="id" class="slide">`;
+        carouselDiv.appendChild(imgDiv);
+    });
+};
+export default createCarousel;

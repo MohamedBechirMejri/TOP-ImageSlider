83a116dc122bfbcffb3884b6129939e6632ec60a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-17 18:00:29 +0100

    clear carousel before adding new one

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,9 +1,12 @@
 export const createCarousel = (array) => {
     const carouselDiv = document.getElementById('carousel');
+    carouselDiv.innerHTML = '';
     array.forEach((element) => {
         const imgDiv = document.createElement('div');
         imgDiv.classList.add('slideDiv');
-        imgDiv.innerHTML = `<img src="${element.url}" data-id="${array.indexOf(element)}" class="slide">`;
+        imgDiv.innerHTML = `<img src="${element.url}" data-id="${array.indexOf(
+      element,
+    )}" class="slide">`;
         carouselDiv.appendChild(imgDiv);
     });
 };

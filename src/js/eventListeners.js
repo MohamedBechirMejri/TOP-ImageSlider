0c0f684049968269b3207f7c85fd75ac5f287343
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-18 22:15:04 +0100

    beautify code

--- src/js/eventListeners.js
+++ src/js/eventListeners.js
@@ -1,27 +1,31 @@
-import { moveToClickedPic, moveToLastPic, moveToNextPic } from './DOM';
+import {
+    moveToClickedPic,
+    moveToLastPic,
+    moveToNextPic
+} from './DOM';
 
 const listen = () => {
-  // eslint-disable-next-line no-unused-vars
-  const toRightButton = (() => {
-    const rightButton = document.getElementById('nav-right');
-    rightButton.addEventListener('click', moveToNextPic);
-  })();
+    // eslint-disable-next-line no-unused-vars
+    const toRightButton = (() => {
+        const rightButton = document.getElementById('nav-right');
+        rightButton.addEventListener('click', moveToNextPic);
+    })();
 
-  // eslint-disable-next-line no-unused-vars
-  const toLeftButton = (() => {
-    const leftButton = document.getElementById('nav-left');
-    leftButton.addEventListener('click', moveToLastPic);
-  })();
+    // eslint-disable-next-line no-unused-vars
+    const toLeftButton = (() => {
+        const leftButton = document.getElementById('nav-left');
+        leftButton.addEventListener('click', moveToLastPic);
+    })();
 
-  // eslint-disable-next-line no-unused-vars
-  const toNavImgs = (() => {
-    const navImgs = document.querySelectorAll('.navslide');
-    navImgs.forEach((img) => {
-      img.addEventListener('click', () => {
-        moveToClickedPic(img.getAttribute('data-id'));
-      });
-    });
-  })();
+    // eslint-disable-next-line no-unused-vars
+    const toNavImgs = (() => {
+        const navImgs = document.querySelectorAll('.navslide');
+        navImgs.forEach((img) => {
+            img.addEventListener('click', () => {
+                moveToClickedPic(img.getAttribute('data-id'));
+            });
+        });
+    })();
 };
 
 export default listen;

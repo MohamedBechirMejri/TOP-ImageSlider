4122d6210270c1813c1f831f88914670cb70c146
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-18 22:02:18 +0100

    beautify

--- src/js/eventListeners.js
+++ src/js/eventListeners.js
@@ -1,17 +1,20 @@
-import { moveToLastPic, moveToNextPic } from './DOM';
+import {
+    moveToLastPic,
+    moveToNextPic
+} from './DOM';
 
 const listen = () => {
-  // eslint-disable-next-line no-unused-vars
-  const toRightButton = (() => {
-    const rightButton = document.getElementById('nav-right');
-    rightButton.addEventListener('click', moveToNextPic);
-  })();
+    // eslint-disable-next-line no-unused-vars
+    const toRightButton = (() => {
+        const rightButton = document.getElementById('nav-right');
+        rightButton.addEventListener('click', moveToNextPic);
+    })();
 
-  // eslint-disable-next-line no-unused-vars
-  const toLeftButton = (() => {
-    const leftButton = document.getElementById('nav-left');
-    leftButton.addEventListener('click', moveToLastPic);
-  })();
+    // eslint-disable-next-line no-unused-vars
+    const toLeftButton = (() => {
+        const leftButton = document.getElementById('nav-left');
+        leftButton.addEventListener('click', moveToLastPic);
+    })();
 };
 
 export default listen;

0e0068665f759f06fd8a145c041aa231494fe741
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-18 21:56:15 +0100

    go back to first image when you reach the last

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,37 +1,40 @@
 const createCarousel = (array) => {
-    const carouselDiv = document.getElementById('imgs-container');
-    carouselDiv.innerHTML = '';
+  const carouselDiv = document.getElementById('imgs-container');
+  carouselDiv.innerHTML = '';
 
-    array.forEach((element) => {
-        const imgDiv = document.createElement('div');
-        imgDiv.classList.add('slideDiv');
-        imgDiv.innerHTML = `<img src="${element.url}" data-id="${array.indexOf(
+  array.forEach((element) => {
+    const imgDiv = document.createElement('div');
+    imgDiv.classList.add('slideDiv');
+    imgDiv.innerHTML = `<img src="${element.url}" data-id="${array.indexOf(
       element,
     )}" class="slide">`;
-        carouselDiv.appendChild(imgDiv);
-    });
+    carouselDiv.appendChild(imgDiv);
+  });
 
-    const bottomNavbar = document.createElement('div');
-    bottomNavbar.classList.add('bottomnavbar');
-    bottomNavbar.innerHTML = carouselDiv.innerHTML;
+  const bottomNavbar = document.createElement('div');
+  bottomNavbar.classList.add('bottomnavbar');
+  bottomNavbar.innerHTML = carouselDiv.innerHTML;
 
-    const navBarSlides = bottomNavbar.querySelectorAll('.slide');
-    navBarSlides.forEach((slide) => {
-        slide.classList.remove('slide');
-        slide.classList.add('navslide');
-    });
+  const navBarSlides = bottomNavbar.querySelectorAll('.slide');
+  navBarSlides.forEach((slide) => {
+    slide.classList.remove('slide');
+    slide.classList.add('navslide');
+  });
 
-    carouselDiv.appendChild(bottomNavbar);
+  carouselDiv.appendChild(bottomNavbar);
 
-    document.querySelector(`[data-id="0"]`).classList.add('visible');
+  document.querySelector(`[data-id="0"]`).classList.add('visible');
 };
 
 export const moveToNextPic = () => {
-    const currentPic = document.querySelector('.visible');
-    const currentPicId = currentPic.getAttribute('data-id');
-    currentPic.classList.remove('visible');
-    const nextPic = document.querySelector(`[data-id="${+currentPicId + 1}"]`);
-    nextPic.classList.add('visible');
+  const slides = document.querySelectorAll('.slide');
+  const currentPic = document.querySelector('.visible');
+  let currentPicId = currentPic.getAttribute('data-id');
+
+  currentPicId == slides.length - 1 ? (currentPicId = '-1') : ''; //refactor this into own function later.
+  currentPic.classList.remove('visible');
+  const nextPic = document.querySelector(`[data-id="${+currentPicId + 1}"]`);
+  nextPic.classList.add('visible');
 };
 
 export default createCarousel;

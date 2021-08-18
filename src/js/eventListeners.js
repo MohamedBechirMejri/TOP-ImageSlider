56368235622942db39386aa4aa9750209cf65d17
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-18 22:01:02 +0100

    listen to left nav

--- src/js/eventListeners.js
+++ src/js/eventListeners.js
@@ -1,11 +1,16 @@
-import { moveToNextPic } from "./DOM";
+import { moveToLastPic, moveToNextPic } from "./DOM";
 
 const listen = () => {
     // eslint-disable-next-line no-unused-vars
     const toRightButton = (() => {
         const rightButton = document.getElementById('nav-right');
         rightButton.addEventListener('click', moveToNextPic)
-  })();
+    })();
+
+        const toLeftButton = (() => {
+          const leftButton = document.getElementById('nav-left');
+          leftButton.addEventListener('click', moveToLastPic);
+        })();
 };
 
 export default listen;

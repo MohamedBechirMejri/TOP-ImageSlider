f0dae7e14d5105d8b683dfbc8ba89da995c043f3
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-19 18:40:48 +0100

    beautify

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import './main.scss';
 
-import createCarousel, { slideShow } from './js/DOM';
+import createCarousel, {
+    slideShow
+} from './js/DOM';
 
 import listen from './js/eventListeners';
 
 listen();
 
 const ImageSlider = (arrayOfImages, timeout) => {
-  createCarousel(arrayOfImages);
-  slideShow(timeout);
+    createCarousel(arrayOfImages);
+    slideShow(timeout);
 };
 
 export default ImageSlider;
